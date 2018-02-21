a3db69947e747e2ba13c11e00bb0d60ec3e61232
kshiprashetty <kshiprashetty@gmail.com>
2018-02-21 12:39:39 +0530

    changes the display to page 2

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -12,43 +12,70 @@ class App extends Component {
       displayPage: 0,
     };
   }
-  render() {
-    if (!this.state.displayPage) {
-      return (
+
+   syncBooks = () => {
+     fetch('/books/booksRating/populate')
+       .then((response) => {
+         if (!response.ok) {
+           console.log('Network request failed');
+         }
+         return response;
+       })
+       .then(data => (data.json()))
+       .then(books => console.log(books));
+
+     fetch('/books/fetchDatabase')
+       .then((response) => {
+         if (!response.ok) {
+           console.log('Network request failed');
+         }
+         return response;
+       })
+       .then(data => (data.json()))
+       .then(books => console.log(books));
+     this.setState({
+       displayPage: 1,
+     });
+     // this.props.changeDisplay(1);
+   }
+
+   render() {
+     if (!this.state.displayPage) {
+       return (
 
 
-        <div className="App" >
-          <div className="SideBar">
-            <header className="SideBarHeader"> {this.props.bs}</header>
-            <div className="SideBarSync">
-              <MaterialIcon icon="sync" color="white" size="32px" />
-            </div>
-            <div className="SideBarSettings">
-              <MaterialIcon icon="settings" color="white" size="32px" />
-            </div>
-          </div>
+         <div className="App" >
+           <div className="SideBar">
+             <header className="SideBarHeader"> {this.props.bs}</header>
+             <div className="SideBarSync">
+               <MaterialIcon icon="sync" color="white" size="32px" />
+             </div>
+             <div className="SideBarSettings">
+               <MaterialIcon icon="settings" color="white" size="32px" />
+             </div>
+           </div>
 
 
-          <div className="MainBody">
-            <div className="MainHeader">
-              <div className="Header">{this.props.header}</div>
-            </div>
-            <div className="MainBodyContainer">
-              <div className="NotFound">
-                <p className="NotFoundText">{this.props.notFoundText}</p>
-                <button className="FirstSync" >
-                  <MaterialIcon icon="sync" color="white" size="32px" />
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      );
-    }
-    return (
-      <div />
-    );
-  }
+           <div className="MainBody">
+             <div className="MainHeader">
+               <div className="Header">{this.props.header}</div>
+             </div>
+             <div className="MainBodyContainer">
+               <div className="NotFound">
+                 <p className="NotFoundText">{this.props.notFoundText}</p>
+                 <button className="FirstSync" onClick={this.syncBooks}>
+                   <MaterialIcon icon="sync" color="white" size="32px" />
+                 </button>
+               </div>
+             </div>
+           </div>
+         </div>
+       );
+     }
+     return (
+       <div />
+     );
+   }
 }
 App.propTypes = {
   header: PropTypes.string,
